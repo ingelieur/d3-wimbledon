a66941636b658880fb3bf4cc4b60c99b2ee074f0
ingelieur <hacktiv8@hacktiv8s-Mac-mini.local>
2017-06-19 13:19:52 +0700

    axes nicely done

--- script.js
+++ script.js
@@ -6,25 +6,20 @@ const width = 750,
   margin = 20,
   marginLeft = 40
 
-const width_space = 15
+const width_space = 16
 
-var goalsScored = []
+var goalsScored = [],
+  opponents = []
 
-var yScale = d3.scaleLinear()
-
-var colorScale = d3.scaleLinear()
+var yScale = d3.scaleLinear(),
+  colorScale = d3.scaleLinear(),
+  xScale = d3.scaleLinear()
 
 // Drawing area
 let svg = d3.select('#results')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
-
-//svg.selectAll('rect')
-//  .data(dataset)
-//  .enter()
-//  .append('rect')
-
 
 // Data reloading
 let reload = () => {
@@ -34,18 +29,21 @@ let reload = () => {
     else {
       data.forEach(datum => {
         goalsScored.push(datum.GoalsScored)
+        opponents.push(datum.Opponent)
       })
       yScale.domain([0, d3.max(goalsScored)])
         .range([0, height])
+      xScale.domain([0, goalsScored.length])
+        .range([0, width-marginLeft])
       colorScale.domain([0, d3.max(goalsScored)])
-        .range(['green', 'red'])
-      redraw(goalsScored)
+        .range(['pink', 'red'])
+      redraw(goalsScored, opponents)
     }
   })
 }
 
 // redraw function
-let redraw = (dataset) => {
+let redraw = (dataset, opponents) => {
   // Your data to graph here
   svg.selectAll('rect')
     .data(dataset)
@@ -53,16 +51,40 @@ let redraw = (dataset) => {
     .append('rect')
     .attr('class', 'bar')
     .attr('x', (d, i) => {
-      return i*width_space
+      console.log(xScale(i))
+      return xScale(i) + marginLeft
     })
     .attr('y', (d, i) => {
-      return height - yScale(d)
+      return height - yScale(d) - margin
     })
-    .attr('width', 13)
+    .attr('width', (d, i) => {
+      return xScale(i+1) - xScale(i) - 2
+    })
     .attr('height', (d) => {
       return yScale(d)
     })
     .attr('fill', colorScale)
+
+  let yAxisScale = yScale.domain([0, d3.max(dataset)])
+    .range([height - margin, 0])
+
+  let y_axis = d3.axisLeft()
+    .scale(yAxisScale)
+
+  svg.append('g')
+    .attr('transform', `translate(${marginLeft})`)
+    .call(y_axis)
+
+  let xAxisScale = d3.scaleLinear()
+    .domain([0, dataset.length])
+    .range([0, width - marginLeft])
+
+  let x_axis = d3.axisBottom()
+    .scale(xAxisScale)
+
+  svg.append('g')
+    .attr('transform', `translate(${marginLeft}, ${height-margin})`)
+    .call(x_axis)
 }
 
 reload()
