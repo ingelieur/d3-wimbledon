e42bf13c5b874a7ad4f5fc61af797cc8980a10d6
ingelieur <hacktiv8@hacktiv8s-Mac-mini.local>
2017-06-19 11:27:22 +0700

    graph is created

--- script.js
+++ script.js
@@ -3,8 +3,16 @@
 // Our canvas
 const width = 750,
   height = 300,
-  margin = 20
-marginLeft = 40
+  margin = 20,
+  marginLeft = 40
+
+const width_space = 15
+
+var goalsScored = []
+
+var yScale = d3.scaleLinear()
+
+var colorScale = d3.scaleLinear()
 
 // Drawing area
 let svg = d3.select('#results')
@@ -12,15 +20,49 @@ let svg = d3.select('#results')
   .attr('width', width)
   .attr('height', height)
 
+//svg.selectAll('rect')
+//  .data(dataset)
+//  .enter()
+//  .append('rect')
+
+
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv('afcw-results.tsv', (error, data) => {
+    if(error) console.log(error)
+    else {
+      data.forEach(datum => {
+        goalsScored.push(datum.GoalsScored)
+      })
+      yScale.domain([0, d3.max(goalsScored)])
+        .range([0, height])
+      colorScale.domain([0, d3.max(goalsScored)])
+        .range(['green', 'red'])
+      redraw(goalsScored)
+    }
+  })
 }
 
 // redraw function
-let redraw = (data) => {
+let redraw = (dataset) => {
   // Your data to graph here
-
+  svg.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d, i) => {
+      return i*width_space
+    })
+    .attr('y', (d, i) => {
+      return height - yScale(d)
+    })
+    .attr('width', 13)
+    .attr('height', (d) => {
+      return yScale(d)
+    })
+    .attr('fill', colorScale)
 }
 
 reload()
